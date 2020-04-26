8d2b968c94e9f6ee5399059b26fd8e15922536ab
Siddhant Chouhan <45875248+sidchn@users.noreply.github.com>
2020-04-26 17:08:04 +0530

    Update datafetcher.py

--- FINAL/datafetcher.py
+++ FINAL/datafetcher.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from tabulate import tabulate 
 import os 
 import numpy as np 
-import matplot as plt 
+
 
 extract_contents = lambda row: [x.text.replace('\n', '') for x in row] 
 URL = 'https://www.mohfw.gov.in/'
@@ -52,14 +52,3 @@ table = tabulate(stats, headers=SHORT_HEADERS)
 print(table) 
 
 
-matplotlib.pyplot.show()
-
-plt.barh(y_pos, performance, align='center', alpha=0.5, 
-				color=(234/256.0, 128/256.0, 252/256.0), 
-				edgecolor=(106/256.0, 27/256.0, 154/256.0)) 
-
-plt.yticks(y_pos, objects) 
-plt.xlim(1,80) 
-plt.xlabel('Number of Cases') 
-plt.title('Corona Virus Cases') 
-plt.show() 
